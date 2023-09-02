c1bcec82ea8c0814b74e939e1171aab8db68bdb1
Gabriel Ribeiro Rodrigues Dessotti <gabrieldessotti@hotmail.com>
2023-09-02 16:00:01 -0300

    x

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,22 @@
+import './CampoTexto.css'
+import { useState } from 'react'
+
+const CampoTexto = (props) => {
+
+    const [valor, setValor] = useState('')
+
+
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+        console.log(valor)
+    }
+
+    return(
+        <div className='campo-texto'>
+            <label>{props.label}</label>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}></input>
+        </div>
+    )
+}
+
+export default CampoTexto;
